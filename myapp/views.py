a65557659e5903a8eb0144690ddc73053c7769e7
Patricio <patriciojimenez838@gmail.com>
2025-09-01 12:01:03 -0400

    crea funciones de retorno con models

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_list_or_404
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("Hello World")
+def index (request):
+    return HttpResponse("Index page")
+
+def hello(request, username):
+    return HttpResponse("<h1>Hello %s</h1>" % username)
 
 def about(request):
     return HttpResponse("About")
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, id):
+    task = get_list_or_404(Task, id=id)
+    return HttpResponse('task: %s' % task.title)
+
+def tasksByName(request, title):
+    task = Task.objects.get(title = title)
+    return HttpResponse('task: %s' % task.title)
