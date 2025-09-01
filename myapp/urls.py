564e85f30e67c389b7f42fbb0ad21c47ce9c4c0c
Patricio <patriciojimenez838@gmail.com>
2025-09-01 12:01:24 -0400

    crea urls para funciones creadas

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello),
-    path('about/', views.about)
+    path('', views.index),
+    path('about/', views.about),
+    path('hello/<str:username>', views.hello),
+    path('projects/', views.projects),
+    path('tasks/<int:id>', views.tasks),
+    path('tasks/name/<str:title>', views.tasksByName)
 ]
