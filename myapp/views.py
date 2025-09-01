0e4824dcfcc99670d84601994c55e3cd8d406f1f
Patricio <patriciojimenez838@gmail.com>
2025-09-01 12:46:58 -0400

    usa render para projects y taks, devolviendo info de models

--- myapp/views.py
+++ myapp/views.py
@@ -1,25 +1,37 @@
 from django.http import HttpResponse
 from .models import Project, Task
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import render
 
 # Create your views here.
 def index (request):
-    return render(request, 'index.html')
+    title = 'Django App!!'
+    return render(request, 'index.html', {
+        'title': title
+    })
 
 def about(request):
-    return render(request, 'about.html')
+    username = 'patricio'
+    return render(request, 'about.html', {
+        'username': username
+    })
 
 def hello(request, username):
     return HttpResponse("<h1>Hello %s</h1>" % username)
 
 def projects(request):
-    projects = list(Project.objects.values())
-    return render(request, 'projects.html')
+    # projects = list(Project.objects.values())
+    projects = Project.objects.all()
+    return render(request, 'projects.html', {
+        'projects': projects
+    })
 
-def tasks(request, id):
-    task = get_list_or_404(Task, id=id)
-    return HttpResponse('task: %s' % task.title)
+#def tasks(request, id):
+#    task = get_list_or_404(Task, id=id)
+#    return HttpResponse('task: %s' % task.title)
 
-def tasksByName(request):
+def tasks(request):
     # task = Task.objects.get(title = title)
-    return render(request, 'tasks.html')
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html', {
+        'tasks': tasks
+    })
