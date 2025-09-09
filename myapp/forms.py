b092e2bf65270b7d450298879866232e3fd436b0
Patricio <patriciojimenez838@gmail.com>
2025-09-09 13:29:20 -0300

    crea formulario de nuevos proyectos

--- myapp/forms.py
+++ myapp/forms.py
@@ -3,3 +3,6 @@ from django import forms
 class CreateNewTask(forms.Form):
     title = forms.CharField(label="Titulo de tarea", max_length=200)
     description = forms.CharField(widget=forms.Textarea ,label="Descripcion de la tarea")
+
+class CreateNewProject(forms.Form):
+    name = forms.CharField(label="Nombre del proyecto", max_length=200)
