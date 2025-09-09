03dadcfc2776a9ecc695455d2d33a70abddd6d92
Patricio <patriciojimenez838@gmail.com>
2025-09-09 13:30:26 -0300

    crea funcion para retornar creacion de nuevos proyectos

--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
 from .models import Project, Task
 from django.shortcuts import render, redirect
-from .forms import CreateNewTask
+from .forms import CreateNewTask, CreateNewProject
 
 # Create your views here.
-def index (request):
+
+
+def index(request):
     title = 'Django App!!'
     return render(request, 'index.html', {
         'title': title
     })
+
 
 def about(request):
     username = 'patricio'
@@ -16,35 +19,51 @@ def about(request):
         'username': username
     })
 
+
 def hello(request, username):
     return HttpResponse("<h1>Hello %s</h1>" % username)
+
 
 def projects(request):
     # projects = list(Project.objects.values())
     projects = Project.objects.all()
-    return render(request, 'projects.html', {
+    return render(request, 'projects/projects.html', {
         'projects': projects
     })
 
-#def tasks(request, id):
+# def tasks(request, id):
 #    task = get_list_or_404(Task, id=id)
 #    return HttpResponse('task: %s' % task.title)
+
 
 def tasks(request):
     # task = Task.objects.get(title = title)
     tasks = Task.objects.all()
-    return render(request, 'tasks.html', {
+    return render(request, 'tasks/tasks.html', {
         'tasks': tasks
     })
+
 
 def create_task(request):
 
     if request.method == 'GET':
-        return render(request, 'create_task.html',{
+        return render(request, 'tasks/create_task.html', {
             'form': CreateNewTask
         })
     else:
-        Task.objects.create(title=request.POST['title'], description=request.POST['description'], project_id=2)
+        Task.objects.create(
+            title=request.POST['title'], description=request.POST['description'], project_id=2)
         return redirect("/tasks/")
 
 
+def create_project(request):
+    if request.method == "GET":
+        return render(request, 'projects/create_project.html', {
+            "form": CreateNewProject
+        })
+    else:
+        project = Project.objects.create(name=request.POST['name'])
+        print(project)
+        return render(request, 'projects/create_project.html', {
+            "form": CreateNewProject
+        })
